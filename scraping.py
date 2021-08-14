8bb4325ef462b925bbe3aa037d441e4f2be8a60c
Jacob Zebker <jacobzebker@jacobs-air.attlocal.net>
2021-08-13 19:58:31 -0700

    teest

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere_images": mission_to_mars_imgs(browser)
     }
 
     # Stop webdriver and return data
@@ -93,9 +94,34 @@ def mars_facts():
     # Assign columns and set index of dataframe
     df.columns=['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
-
+    df.dropna(axis=0)
+    
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def mission_to_mars_imgs(browser):
+    # 1. Use browser to visit the URL and parse HTML
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    html = browser.html
+    ssoup = soup(html, 'html.parser')
+    # 2. Create a list to hold the images and titles (img_pages is an intermediate url, there are 2 searches performed).
+    hemisphere_image_urls = []
+    img_pages = []
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # 3a. Find page with full image
+    for x in ssoup.find_all('a',class_='itemLink product-item'):
+        url_img = 'https://astrogeology.usgs.gov'+x.get("href")
+        if url_img not in img_pages:
+            img_pages.append(url_img)
+    # 3b. Navigate to image page and get urls and titles
+    for address in img_pages:
+        browser.visit(address)
+        a = browser.html
+        a = soup(a,'html.parser')
+        hemisphere_image_urls.append({'img_url':a.find('a',target='_blank',string="Sample").get('href'),'title':a.find('h2',class_='title').string})
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
